62d414383c4083de9b0732fd6f80d5cf30f08b27
TGamerxd <90946491+TGamerxd@users.noreply.github.com>
2023-01-18 08:35:14 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var typed = new Typed(".typing", {
+        strings: ["IT Enthusiast ", "Programmer ","Musician ","Gamer "," Python Developer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    })
+})
